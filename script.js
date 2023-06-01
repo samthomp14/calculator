7f06f9fb23c9dddea0f965bdd757bbdbcc693395
Samuel Thompson <134101570+samthomp14@users.noreply.github.com>
2023-05-31 23:00:24 -0400

    Add ability to perform operations but only one at a time. Fix some bugs with decimal button

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let num1 = 0;
 let operator;
-let num2
+let num2 = 0;
 const display = document.querySelector('.text');
+let clearOnNext = false;
 
-function add(a, b) {
+
+function addNum(a, b) {
     return a + b;
 }
 
@@ -24,7 +26,7 @@ function operate(a, op, b) {
     //TODO: try to use op as a callback function?
     switch (op) {
         case 'add':
-            ans = add(a, b);
+            ans = addNum(a, b);
             break;
 
         case 'subtract':
@@ -38,6 +40,8 @@ function operate(a, op, b) {
         case 'divide':
             ans = divide(a, b);
             break;
+        default:
+            ans = num2;
     }
     return ans;
 }
@@ -46,12 +50,17 @@ const numberButtons = document.querySelectorAll('.row button');
 numberButtons.forEach(button => button.addEventListener('click', changeDisplay));
 
 function changeDisplay(e) {
+    if (clearOnNext) {
+        display.textContent = '0';
+        clearOnNext = false;
+        operationButtons.forEach(btn => btn.classList.remove('activate'));
+    }
     if (display.textContent.replace('.','').replace('-','').length < 9){
         if (e.target.textContent == '.') {
-            e.target.removeEventListener('click', changeDisplay); // deactivate decimal point
-        }
-    
-        if (display.textContent == '0') {
+            if(!display.textContent.includes('.')) {
+                display.textContent += e.target.textContent;
+            }
+        } else if (display.textContent == '0') {
             display.textContent = e.target.textContent;
         } else {
             display.textContent += e.target.textContent;
@@ -62,7 +71,11 @@ function changeDisplay(e) {
 document.querySelector('#clear').addEventListener('click', clearDisplay);
 
 function clearDisplay(e) {
+    num1 = 0;
+    num2 = 0;
+    operator = 'none';
     display.textContent = '0';
+    operationButtons.forEach(btn => btn.classList.remove('activate'));
     document.querySelector('#decimal').addEventListener('click', changeDisplay);
 }
 
@@ -74,4 +87,24 @@ document.querySelector('#sign').addEventListener('click', () => {
     }
 });
 
+const operationButtons = document.querySelectorAll('.operations .math button');
 
+operationButtons.forEach(button => button.addEventListener('click', recordOp));
+
+function recordOp(e) {
+    const button = e.target
+    num1 = +display.textContent;
+    operator = (button.id);
+    operationButtons.forEach(btn => btn.classList.remove('activate'));
+    button.classList.add('activate');
+    clearOnNext = true;
+}
+
+const equalButton = document.querySelector('#equals');
+equalButton.addEventListener('click', doMath);
+
+function doMath(e) {
+    num2 = +display.textContent;
+    display.textContent = operate(num1, operator, num2);
+    clearOnNext = true;
+}
